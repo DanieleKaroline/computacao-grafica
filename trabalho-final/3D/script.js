aa89764ad939fc69501b03e736d3f35ac1252ef3
DanieleKaroline <carvalho.danielekr@gmail.com>
2024-06-22 22:27:36 -0300

    add 3d

---
+++ trabalho-final/3D/script.js
@@ -0,0 +1,19 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.136.0/build/three.module.js';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.136.0/examples/jsm/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+
+function animate() {
+    requestAnimationFrame(animate);
+
+
+    renderer.render(scene, camera);
+}
+
+animate();
